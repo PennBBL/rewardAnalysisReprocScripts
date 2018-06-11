c88249240a93e8021c86185afff20828f491f677
Lauren Beard <lbeard@sas.upenn.edu>
2018-06-11 14:08:23 -0400

    ravens qa vals

--- ravens/output_Ravens_MeanGM_CT.sh
+++ ravens/output_Ravens_MeanGM_CT.sh
@@ -1,11 +1,10 @@
-scanids=$(cat /data/joy/BBL/projects/rewardAnalysisReproc/subjectLists/ravens/n324_rewardRavens.csv); 
+subjects=$(cat /data/jux/BBL/projects/rewardAnalysisReproc/xnatDownload2/n489_bblid_datexscanid_scanid.csv); 
 
-for i in $scanids; do 
-    id=$(echo $i|cut -d',' -f1); 
-    sc=$(echo $i|cut -d',' -f2);
-    sc=$(echo $sc|cut -d'x' -f2);
-    meanCT=`cat /data/joy/BBL/studies/reward/processedData/structural/ravens/${id}/*${sc}/*MeanGM_CT_RAVENS2.txt`;
-    spaCorr=`fslcc -p 10 --noabs /data/joy/BBL/studies/reward/processedData/structural/ravens/${id}/*${sc}/*_templateInSubjectSpace.nii.gz /data/joy/BBL/studies/reward/processedData/structural/struct_pipeline_201705311006/${id}/*${sc}/antsCT/*ExtractedBrain0N4.nii.gz`;
+for i in $subjects; do 
+    bblid=$(echo $i|cut -d',' -f1); 
+    datexscanid=$(echo $i|cut -d',' -f2);
+    meanCT=`cat /data/joy/BBL/studies/reward/processedData/structural/ravens/${bblid}/${datexscanid}/*MeanGM_CT_RAVENS2.txt`;
+    spaCorr=`fslcc -p 10 --noabs /data/joy/BBL/studies/reward/processedData/structural/ravens/${bblid}/${datexscanid}/*_templateInSubjectSpace.nii.gz /data/joy/BBL/studies/reward/processedData/structural/struct_pipeline_201705311006/${bblid}/${datexscanid}/antsCT/*ExtractedBrain0N4.nii.gz`;
     spaCorr=$(echo $spaCorr | cut -d' ' -f3);
-echo ${id},${sc},${meanCT},${spaCorr} >> /data/joy/BBL/projects/rewardAnalysisReproc/subjectLists/ravens/n324_Ravens_MeanCT.csv; 
+echo ${bblid},${datexscanid},${meanCT},${spaCorr} >> /data/jux/BBL/projects/rewardAnalysisReproc/xnatDownload2/n489_Ravens_MeanCT.csv; 
 done
